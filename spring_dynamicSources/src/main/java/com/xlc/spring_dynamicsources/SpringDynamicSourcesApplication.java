77953b729cb5a8a6a0eb4642d850fbb193d8b8c4
xiaoliangcheng <2637341276@qq.com>
2024-12-14 12:37:26 +0800

    提交dynamic source init module

---
+++ spring_dynamicSources/src/main/java/com/xlc/spring_dynamicsources/SpringDynamicSourcesApplication.java
@@ -0,0 +1,13 @@
+package com.xlc.spring_dynamicsources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamicSourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDynamicSourcesApplication.class, args);
+    }
+
+}
---
+++ spring_dynamicSources/src/test/java/com/xlc/spring_dynamicsources/SpringDynamicSourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlc.spring_dynamicsources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamicSourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
